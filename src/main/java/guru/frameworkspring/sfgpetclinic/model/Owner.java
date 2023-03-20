cbe6999aa92d74733340f407e86a8daf2757a523
AgustinMorel <morelagustin2@gmail.com>
2023-03-20 19:09:40 -0300

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/frameworkspring/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.frameworkspring.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/frameworkspring/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.frameworkspring.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
